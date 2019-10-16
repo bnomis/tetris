66542276c5dd5b1867a2b25b557e8fde6b9ee2b8
Simon Blanchard <bnomis@gmail.com>
2019-10-17 06:42:30 +0800

    add reset.css

--- src/clj/tetris/css.clj
+++ src/clj/tetris/css.clj
@@ -2,6 +2,15 @@
   (:require [garden.def :refer [defstyles]]))
 
 (defstyles screen
-  [:body {:color "red" :background-color "blue"}]
+  [:body
+   {
+     :font "14px 'Helvetica Neue', Helvetica, Arial, sans-serif"
+     :line-height "1.4em"
+     :background "#f5f5f5"
+     :color "#4d4d4d"
+     :-webkit-font-smoothing "antialiased"
+     :-moz-font-smoothing "antialiased"
+     :font-smoothing "antialiased"
+     :font-weight "300"}]
   [:svg#board {:background-color "black"}]
   [:.block-outline {:stroke "#444" :stroke-width "1px"}])

990941271d48c7e6c0dd8b719dea2b7b11fc8835
Simon Blanchard <bnomis@gmail.com>
2019-10-17 06:42:08 +0800

    group grid lines

--- src/cljs/tetris/views.cljs
+++ src/cljs/tetris/views.cljs
@@ -6,9 +6,11 @@
    [:h1 "Tetris"]
    [:svg#board {:width d/board-width :height d/board-height}
     [:rect.block-outline {:x 0 :y 0 :width d/board-width :height d/board-height}]
-    (for [column (range 1 d/board-columns)]
-     (let [x (d/col->px column)]
-       [:line.block-outline {:x1 x :y1 0 :x2 x :y2 d/board-height :key (str "board-column-outline-" column)}]))
-    (for [row (range 1 d/board-rows)]
+    [:g
+      (for [column (range 1 d/board-columns)]
+       (let [x (d/col->px column)]
+         [:line.block-outline {:x1 x :y1 0 :x2 x :y2 d/board-height :key (str "board-column-outline-" column)}]))]
+    [:g
+     (for [row (range 1 d/board-rows)]
       (let [y (d/row->px row)]
-        [:line.block-outline {:x1 0 :y1 y :x2 d/board-width :y2 y :key (str "board-row-outline-" row)}]))]])
+        [:line.block-outline {:x1 0 :y1 y :x2 d/board-width :y2 y :key (str "board-row-outline-" row)}]))]]])
